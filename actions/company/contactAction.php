3f092bf8902d187b17cbc40fac3aca4eacdb0025
tomsisbos <sisbos@free.fr>
2023-05-12 17:30:27 +0900

    Contact form confirmation mail sending

--- actions/company/contactAction.php
+++ actions/company/contactAction.php
@@ -16,7 +16,7 @@ if (isset($_POST) && !empty($_POST)) {
             $address = htmlspecialchars($_POST['email']);
             $content = nl2br(htmlspecialchars($_POST['content']));
 
-            // Send mail
+            // Send mail to contact@cyclingfriends.co
             $email = new Mail();
             $email->setFrom(
                 'contact@cyclingfriends.co',
@@ -34,6 +34,33 @@ if (isset($_POST) && !empty($_POST)) {
             );
             $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
             $response = $sendgrid->send($email);
+
+            // Send verification mail to sender
+            $email = new Mail();
+            $email->setFrom(
+                'contact@cyclingfriends.co',
+                'CyclingFriends'
+            );
+            $email->setSubject('お問い合わせありがとうございます。');
+            $email->addTo($address);
+            $email->addContent(
+                'text/html',
+                "
+                <p>この度、お問い合わせ頂きありがとうございます。問い合わせの内容を下記の通り受付させて頂きました。</p>
+                <p>
+                    <strong>姓：</strong>" .$lastname. "<br>
+                    <strong>名：</strong>" .$firstname. "<br>
+                    <strong>メール：</strong>" .$address. "<br>
+                    <strong>件名：</strong>" .$title. "
+                </p>
+                <br>" .$content. "
+                <p>---</p>
+                <p>近日中にご回答させて頂きます。</p>
+                <p>株式会社テラインコグニタ　スタッフ一同</p>"
+            );
+            $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+            $response = $sendgrid->send($email);
+
 
             $successmessage = "お問い合わせの内容が送信されました。近日中にご対応させて頂きます。";
 
