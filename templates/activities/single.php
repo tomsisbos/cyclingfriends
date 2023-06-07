bc5ee9e70da6e8c81bfd4d4f3447559b3c110c41
tomsisbos <sisbos@free.fr>
2023-06-07 21:20:14 +0900

    Display 'min' for duration running < 1h

--- templates/activities/single.php
+++ templates/activities/single.php
@@ -79,10 +79,16 @@ include '../includes/head.php'; ?>
 				<div class="pg-ac-spec-container front <?= $activity->setBackgroundColor('duration')?>">
 					<div class="pg-ac-spec-label">活動時間</div>
 					<div class="pg-ac-spec-value"> <?php
-						if (substr($activity->duration_running->h, 0, 1) == '0') echo substr($activity->duration_running->h, 1, strlen($activity->duration_running->h));
-						else echo $activity->duration_running->h ?>
-						<span class="my-pg-ac-spec-unit"> h </span>
-						<?= $activity->duration_running->i ?>
+						if (intval($activity->duration_running->h) > 0) {
+							if (substr($activity->duration_running->h, 0, 1) == '0') echo substr($activity->duration_running->h, 1, strlen($activity->duration_running->h));
+							else echo $activity->duration_running->h ?>
+							<span class="my-pg-ac-spec-unit"> h </span>
+							<?= $activity->duration_running->i;
+						} else {
+							if (substr($activity->duration_running->i, 0, 1) == '0') echo substr($activity->duration_running->i, 1, strlen($activity->duration_running->i));
+							else echo $activity->duration_running->i; ?>
+							<span class="my-pg-ac-spec-unit"> min </span> <?php
+						} ?>
 					</div>
 				</div>
 				<div class="pg-ac-spec-container back <?= $activity->setBackgroundColor('break_time')?>" style="display: none;">
