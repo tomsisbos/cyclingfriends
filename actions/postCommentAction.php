8fc30edeefa29e23324e6e5eef7193891962f931
tomsisbos <sisbos@free.fr>
2023-06-19 19:36:43 +0900

    Also notify prev. comment authors on new comment

--- actions/postCommentAction.php
+++ actions/postCommentAction.php
@@ -5,4 +5,5 @@ if (isset($_POST['comment']) AND !empty($_POST['comment'])) {
     $object->postComment($connected_user->id, $content);
     $successmessage = "コメントが投稿されました！";
     if ($connected_user->id != $object->user_id) $object->notify($object->user_id, 'activity_new_comment', $connected_user->id);
+    foreach ($object->getComments() as $comment) $object->notify($comment->user->id, 'activity_new_comment', $connected_user->id);
 }
