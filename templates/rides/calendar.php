6c9e3e58705bef8387732274bb3b9f1617e81178
tomsisbos <sisbos@free.fr>
2023-08-29 09:03:27 +0900

    BF: Typo correction

--- templates/rides/calendar.php
+++ templates/rides/calendar.php
@@ -20,7 +20,7 @@ include '../includes/head.php'; ?>
 
         <p>年間を通じて、地方の魅力を探索するサイクリングツアーを開催しております。</p>
 
-        <p>CyclingFriendsのロゴマークの四色が意味している、「<strong>景色</strong>、<strong>食</strong>、<strong>分化</strong>、<strong>仲間</strong>」の４柱を主軸に、思い出に深く残るコンテンツづくりにこだわっています。</p>
+        <p>CyclingFriendsのロゴマークの四色が意味している、「<strong>景色</strong>、<strong>食</strong>、<strong>文化</strong>、<strong>仲間</strong>」の４柱を主軸に、思い出に深く残るコンテンツづくりにこだわっています。</p>
 
         <p>お好きなツアー名をクリックすると、詳細ページにアクセスできます。参加するには、アカウントを新規作成／ログインの上、詳細ページに表示されている「参加」ボタンをクリックし、必要情報を入力して頂くだけです。</p>
         
