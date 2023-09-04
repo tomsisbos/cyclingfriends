cd71ae54e304dbcd9a2fbd31c6444c299750d6f3
tomsisbos <sisbos@free.fr>
2023-09-04 11:25:50 +0900

    BF: Activity comment notif. was sent to author

--- actions/postComment.php
+++ actions/postComment.php
@@ -1,9 +1,17 @@
 <?php
 
 if (isset($_POST['comment']) AND !empty($_POST['comment'])) {
+
+    // Post comment
     $content = $_POST['content'];
     $object->postComment(getConnectedUser()->id, $content);
     $successmessage = "コメントが投稿されました！";
+
+    // Notify object author
     if (getConnectedUser()->id != $object->user_id) $object->notify($object->user_id, 'activity_new_comment', getConnectedUser()->id);
-    foreach ($object->getComments() as $comment) $object->notify($comment->user->id, 'activity_new_comment', getConnectedUser()->id);
+
+    // Notify other comment posters
+    foreach ($object->getComments() as $comment) {
+        if ($comment->user->id != getConnectedUser()->id) $object->notify($comment->user->id, 'activity_new_comment', getConnectedUser()->id);
+    }
 }
